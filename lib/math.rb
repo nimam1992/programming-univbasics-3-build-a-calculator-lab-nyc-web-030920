8322327e5e6b595a8f1a980a186d91b6efa7e171
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-12 16:48:03 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  equation = num1+num2
+  return equation
 end
 
 def subtraction(num1, num2)
-
+  equation = num1-num2
+  return equation
 end
 
 def division(num1, num2)
-
+  equation = num1/num2
+  return equation
 end
 
 def multiplication(num1, num2)
-
+  equation = num1*num2
+  return equation
 end
 
 def modulo(num1, num2)
-
+  equation = num1%num2
+  return equation
 end
 
 def square_root(num)
-
+return Math.sqrt(num)
 end
